72244918c07a90e557abd071b1210519d07c42bb
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-26 17:19:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root', //
-        password: '',
-        database: 'screeningstar',
+        host: 'srv871.hstgr.io',
+        user: 'u510451310_Screeningstar', 
+        password: 'U510451310_Screeningstar',
+        database: 'u510451310_Screeningstar',
     },
     jwtSecret: 'screeningstar@2024',
 };
