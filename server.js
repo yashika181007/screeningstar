5d875788005080e1ed3633763198ed583e795d9a
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-26 18:31:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const config = require('./config');
+const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-
+app.use(cors()); 
 const app = express();
 
 app.use(bodyParser.json());
