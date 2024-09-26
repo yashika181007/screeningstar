dc00fc716516801b0196530b34f3587551704a15
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-26 18:40:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use('/Screeningstar', authRoutes);
 
 // Set the port from environment variables or default to 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Start the server
 app.listen(PORT, () => {
