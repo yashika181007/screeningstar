f083eb3ae97aacbdd399c47c7f44e33a19106bc0
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-26 18:50:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Import required packages
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
@@ -6,22 +5,16 @@ const config = require('./config');
 const cors = require('cors');
 require('dotenv').config();
 
-// Create an Express application
 const app = express();
 
-// Enable CORS
 app.use(cors());
 
-// Middleware to parse incoming JSON requests
 app.use(bodyParser.json());
 
-// Define your API routes
 app.use('/Screeningstar', authRoutes);
 
-// Set the port from environment variables or default to 5000
 const PORT = process.env.PORT;
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
