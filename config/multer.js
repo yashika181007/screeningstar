eaa524338463cd783ec7e712b1af7277cc5feae0
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-27 18:43:36 +0530

    ds

--- config/multer.js
+++ config/multer.js
@@ -1,7 +1,9 @@
 const multer = require('multer');
 const path = require('path');
 const storage = multer.diskStorage({
-    destination: '../uploads/', 
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
     filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
     }
