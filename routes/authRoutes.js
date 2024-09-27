7e8cbf113290b316d4a766d4ee81743c4402a80d
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-27 18:29:18 +0530

    d

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const { uploaduserphoto } = require('../config/multer'); // Adjust the path to your multer file
 
-// Route to create a user with photo upload
-router.post('/createuser', uploaduserphoto, authController.createuser); // Add upload middleware here
+router.post('/createuser', authController.createuser);
 router.post('/login', authController.login);
 router.get('/users', authController.getAllUsers);
 router.get('/users/:id', authController.getUserById);
 router.put('/users/:id', authController.updateUser);
 router.delete('/users/:id', authController.deleteUser);
-
 module.exports = router;
