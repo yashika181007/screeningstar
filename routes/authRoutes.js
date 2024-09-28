3f5f5c604040bb0e9851a7dfc7853a0b987122dd
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-28 19:09:17 +0530

    90

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,8 +4,8 @@ const authController = require('../controllers/authController');
 
 router.post('/createuser', authController.createuser);
 router.post('/login', authController.login);
-router.get('/users', authController.getAllUsers);
-router.get('/users/:id', authController.getUserById);
-router.put('/users/:id', authController.updateUser);
-router.delete('/users/:id', authController.deleteUser);
+router.get('/users', authController.getAllUsers,authenticateToken);
+router.get('/users/:id', authController.getUserById,authenticateToken);
+router.put('/users/:id', authController.updateUser,authenticateToken);
+router.delete('/users/:id', authController.deleteUser,authenticateToken);
 module.exports = router;
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
 
-router.post('/clients', clientController.createClient);
+router.post('/clients', clientController.createClient,authenticateToken);
 
-router.get('/clients', clientController.getClients);
+router.get('/clients', clientController.getClients,authenticateToken);
 
-router.get('/clients/:id', clientController.getClientById);
+router.get('/clients/:id', clientController.getClientById,authenticateToken);
 
-router.put('/clients/:id', clientController.updateClient); 
+router.put('/clients/:id', clientController.updateClient,authenticateToken); 
 
-router.delete('/clients/:id', clientController.deleteClient);
+router.delete('/clients/:id', clientController.deleteClient,authenticateToken);
 
 module.exports = router;
