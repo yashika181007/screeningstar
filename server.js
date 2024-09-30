a3b18db70c22da039aed8514be9044042187ed27
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-30 18:12:34 +0530

    1

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: 'Something went wrong!', error: err.message });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
