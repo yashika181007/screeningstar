a19ead715db90177bc09961cedf2310d6572d3be
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-30 11:48:27 +0530

    q

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -7,7 +7,8 @@ router.post('/clients', clientController.createClient);
 router.get('/clients', clientController.getClients);
 
 router.get('/clients/:id', clientController.getClientById);
-
+router.get('/clients/active', clientController.getActiveClients);
+router.get('/clients/inactive', clientController.getInactiveClients);
 router.put('/clients/:id', clientController.updateClient); 
 
 router.delete('/clients/:id', clientController.deleteClient);
