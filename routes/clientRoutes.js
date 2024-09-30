e8867598f63c27ec982b33e1d1ce81ddbf6ac977
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-09-30 12:19:25 +0530

    8

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -2,15 +2,9 @@ const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
 
-router.post('/clients', clientController.createClient);
-
-router.get('/clients', clientController.getClients);
-
-router.get('/clients/:id', clientController.getClientById);
+;
 router.get('/clients/active', clientController.getActiveClients);
 router.get('/clients/inactive', clientController.getInactiveClients);
-router.put('/clients/:id', clientController.updateClient); 
 
-router.delete('/clients/:id', clientController.deleteClient);
 
 module.exports = router;
