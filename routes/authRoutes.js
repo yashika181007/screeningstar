94c8ef212bbbe4eb17f8956f0320c71b0f926b13
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-01 17:27:29 +0530

    123

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,11 @@ const authController = require('../controllers/authController');
 const verifyToken = require('../config/verifyToken');
 
 router.post('/createuser', verifyToken, authController.createuser);
-router.post('/login', authController.login);
+
+router.get('/login', verifyToken,uthController.login, (req, res) => {
+    res.status(200).json({ message: 'Access granted', userId: req.id, userRole: req.role });
+});
+
 router.get('/users', verifyToken, authController.getAllUsers);
 router.get('/users/active', verifyToken, authController.getActiveUsers);
 router.get('/users/inactive', verifyToken, authController.getInactiveUsers);
