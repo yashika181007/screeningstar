511087c417073eb8eed7e62d56a0261edd83b0b9
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-01 12:32:14 +0530

    1

--- config/verifyToken.js
+++ config/verifyToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     const token = req.headers['authorization'];
-
+    console.log('Incoming request headers:', req.headers);
     if (!token) {
         return res.status(403).json({ message: 'No token provided.' });
     }
@@ -11,9 +11,12 @@ const verifyToken = (req, res, next) => {
     if (tokenParts[0] !== 'Bearer' || !tokenParts[1]) {
         return res.status(401).json({ message: 'Invalid token format.' });
     }
+   
+    console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
     jwt.verify(tokenParts[1], process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
+            console.log('Token verification error:', err);
             return res.status(401).json({ message: 'Unauthorized!' });
         }
 
