1a4d8bc78394ecbb4b3e04ab653eef94e550ff2b
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-01 16:34:23 +0530

    again update token

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const verifyToken = require('../config/verifyToken');
 const clientController = require('../controllers/clientController');
 
 router.post('/clients',verifyToken, clientController.createClient);
