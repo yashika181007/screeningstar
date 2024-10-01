8b918d80728cb5851241feb9174504f8dcbf83f9
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-01 12:06:26 +0530

    123

--- config/verifyToken.js
+++ config/verifyToken.js
@@ -2,23 +2,22 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     const token = req.headers['authorization'];
+
     if (!token) {
-        console.log('No token provided.');
         return res.status(403).json({ message: 'No token provided.' });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    const tokenParts = token.split(' ');
+    if (tokenParts[0] !== 'Bearer' || !tokenParts[1]) {
+        return res.status(401).json({ message: 'Invalid token format.' });
+    }
+
+    jwt.verify(tokenParts[1], process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            console.log('Token verification failed:', err.message);
             return res.status(401).json({ message: 'Unauthorized!' });
         }
-        
-        console.log('Token successfully verified:', decoded);
 
         req.userId = decoded.id;
-
-        console.log('Session created for userId:', req.userId);
-
         next();
     });
 };
