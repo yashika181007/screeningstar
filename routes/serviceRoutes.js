ab4b10d3d097bb83ad5fc2b46835a52d2f427303
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-01 12:15:50 +0530

    1234

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const serviceRoutes = require('../controllers/serviceRoutes');
+const serviceController = require('../controllers/serviceController');
 
 router.post('/service', serviceController.createService);
 router.get('/service', serviceController.getAllServices);
