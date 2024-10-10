482c2c9e9c2e1a673694b63adb60082f121d574e
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-10 16:56:34 +0530

    1

--- models/ClientSpoc.js
+++ models/ClientSpoc.js
@@ -76,3 +76,4 @@ const ClientSpoc = sequelize.define('clientspoc', {
 sequelize.sync()
     .then(() => console.log('Database & tables created!'))
     .catch(error => console.error('Error creating database:', error));
+    module.exports = ClientSpoc;
