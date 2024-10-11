8058c494fc555e081dc85441fb3ecbcf2073ffd1
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-11 11:55:57 +0530

    billingspocController

--- routes/billingspocRoutes.js
+++ routes/billingspocRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const verifyToken = require('../config/verifyToken');
 const billingspocController = require('../controllers/billingspocController');
 
-router.post('/billingspoc',verifyToken ,BillingSpocController.createBillingSpoc);
-router.get('/billingspoc',verifyToken, BillingSpocController.getAllBillingSpoc);
-router.get('/billingspoc/:id',verifyToken, BillingSpocController.getallBillingSpocById);
-router.put('/billingspoc/:id',verifyToken, BillingSpocController.updateBillingSpoc);
-router.delete('/billingspoc/:id',verifyToken, BillingSpocController.deleteBillingSpoc);
+router.post('/billingspoc',verifyToken ,billingspocController.createBillingSpoc);
+router.get('/billingspoc',verifyToken, billingspocController.getAllBillingSpoc);
+router.get('/billingspoc/:id',verifyToken, billingspocController.getallBillingSpocById);
+router.put('/billingspoc/:id',verifyToken, billingspocController.updateBillingSpoc);
+router.delete('/billingspoc/:id',verifyToken, billingspocController.deleteBillingSpoc);
 
 module.exports = router;
