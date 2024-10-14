a54a31758de901f9e3bcfc8721dc3b883b3e4545
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-14 17:51:04 +0530

    const Branch = require('../models/Branch');

--- models/Client.js
+++ models/Client.js
@@ -1,6 +1,7 @@
 const Users = require('../models/User');
 const { Sequelize, DataTypes } = require('sequelize');
 const config = require('../config');
+const Branch = require('../models/Branch');
 
 const sequelize = new Sequelize(config.database.database, config.database.user, config.database.password, {
     host: config.database.host,
