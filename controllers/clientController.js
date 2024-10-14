f087bf6ee67ad5c414a3dd5723373bf09d9afe0e
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-14 17:58:26 +0530

    define branch in client controller

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,4 +1,5 @@
 const Client = require('../models/Client');
+const Client = require('../models/Branch');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
