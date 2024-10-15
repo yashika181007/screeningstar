f8647c196689b1f68227f7d07a0856eda41c1875
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-15 11:13:03 +0530

    1

--- models/Client.js
+++ models/Client.js
@@ -1,3 +1,12 @@
+const Users = require('../models/User');
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../config');
+const Branch = require('../models/Branch');
+
+const sequelize = new Sequelize(config.database.database, config.database.user, config.database.password, {
+    host: config.database.host,
+    dialect: 'mysql',
+});
 const Client = sequelize.define('client', {
     id: {
         type: DataTypes.INTEGER,
