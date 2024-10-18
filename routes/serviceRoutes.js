9cd82af3bfe8cacccc5b1ba8400050db5e1b1a5b
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-18 10:30:26 +0530

    servicetest

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -5,6 +5,7 @@ const serviceController = require('../controllers/serviceController');
 
 router.post('/service',verifyToken, serviceController.createService);
 router.get('/service',verifyToken, serviceController.getAllServices);
+router.get('/servicetest', serviceController.getAllServices);
 router.get('/service/:id',verifyToken, serviceController.getServiceById);
 router.put('/service/:id',verifyToken, serviceController.updateService);
 router.delete('/service/:id',verifyToken, serviceController.deleteService);
