ade6a1f30967cda34fa2c6c0880ac134b9222a9c
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-10-29 14:03:44 +0530

    def

--- routes/candidatemanagerRoutes.js
+++ routes/candidatemanagerRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../config/verifyToken');
 const candidatemanagerController = require('../controllers/candidatemanagerController');
-const digitalAVController = require('../controllers/digitalAVController');
 
 router.post('/candidatemanager', verifyToken, candidatemanagerController.createcandidatemanager);
 router.get('/candidatemanager', verifyToken, candidatemanagerController.getAllCandidateManagers);
@@ -10,10 +9,10 @@ router.get('/candidatemanager/:id', verifyToken, candidatemanagerController.getC
 router.put('/candidatemanager/:id', verifyToken, candidatemanagerController.updateCandidateManager);
 router.delete('/candidatemanager/:id', verifyToken, candidatemanagerController.deleteCandidateManager);
 
-router.post('/digitalav',verifyToken, digitalAVController.createDigitalAV);
-router.get('/digitalav',verifyToken, digitalAVController.getAllDigitalAV);
-router.get('/digitalav/:id',verifyToken, digitalAVController.getDigitalAVById);
-router.put('/digitalav/:id',verifyToken, digitalAVController.updateDigitalAV);
-router.delete('/digitalav/:id',verifyToken, digitalAVController.deleteDigitalAV);
+router.post('/digitalav',verifyToken, candidatemanagerController.createDigitalAV);
+router.get('/digitalav',verifyToken, candidatemanagerController.getAllDigitalAV);
+router.get('/digitalav/:id',verifyToken, candidatemanagerController.getDigitalAVById);
+router.put('/digitalav/:id',verifyToken, candidatemanagerController.updateDigitalAV);
+router.delete('/digitalav/:id',verifyToken, candidatemanagerController.deleteDigitalAV);
 
 module.exports = router;
