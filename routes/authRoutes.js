9a7737ca148816a0408b5be92c8d4e644e08d5d3
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-11-04 13:01:34 +0530

    sd

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,17 +12,17 @@ router.post('/createuser', upload.single('employeePhoto'), (req, res) => {
         return res.status(400).json({ message: 'No file uploaded.' });
     }
 
-    const filePath = `uploads/${req.file.filename}`; // Define filePath based on uploaded file
+    const filePath = `uploads/${req.file.filename}`;
 
-    // Configure Git user details if not already set (one-time setup per environment)
+    // Configure Git user details if not already set
     exec('git config --global user.email "yashikawebstep@gmail.com" && git config --global user.name "Yashika"', (err, stdout, stderr) => {
         if (err) {
             console.error('Error configuring Git user:', err);
             return res.status(500).json({ message: 'Failed to configure Git user', error: err });
         }
-        
-        // Run the Git commands to add, commit, and push the uploaded file
-        exec(`git add -f ${filePath} && git commit -m "Auto-commit: Add new image ${req.file.filename}" && git push origin main`, (error, stdout, stderr) => {
+
+        // Run the Git commands to add, commit, and push the uploaded file to `master`
+        exec(`git add -f ${filePath} && git commit -m "Auto-commit: Add new image ${req.file.filename}" && git push origin master`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error pushing to GitHub: ${error}`);
                 return res.status(500).json({ message: 'Failed to push image to GitHub', error });
@@ -33,7 +33,7 @@ router.post('/createuser', upload.single('employeePhoto'), (req, res) => {
         });
     });
 });
-router.post('/login',  authController.login);
+router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/verif-login', verifyToken, authController.veriflogin);
 // router.post('/createuser', upload.single('employeePhoto'),authController.createuser);
@@ -45,7 +45,7 @@ router.get('/users/inactive', verifyToken, authController.getInactiveUsers);
 router.get('/users/:id', verifyToken, authController.getUserById);
 router.put('/users/:id', verifyToken, authController.updateUser);
 router.delete('/users/:id', verifyToken, authController.deleteUser);
-router.put('/users/status/:id',verifyToken, authController.changeUserStatus);
-router.post('/logout',verifyToken, authController.logout);
+router.put('/users/status/:id', verifyToken, authController.changeUserStatus);
+router.post('/logout', verifyToken, authController.logout);
 
 module.exports = router;
