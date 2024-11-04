e0cacc81e0fc1d1734279057dbd6ad1a10cd5bd9
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2024-11-04 15:16:47 +0530

    qswq

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ const upload = require('../config/multer');
 const simpleGit = require('simple-git');
 const path = require('path');
 
-const git = simpleGit('/'); // Set this to the path of your Git repository
+const git = simpleGit(path.join(__dirname, '../uploads/screeningstar'));
 
 router.post('/createuser', upload.single('employeePhoto'), async (req, res) => {
     if (!req.file) {
