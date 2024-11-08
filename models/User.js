6d530d19c5d6a59deadc551bc50d96c0d47acc7f
rohitwebstep <rohitwebstep@gmail.com>
2024-11-08 12:18:33 +0530

    add login_expiry in user model

--- models/User.js
+++ models/User.js
@@ -1,53 +1,67 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const config = require('../config');
+const { Sequelize, DataTypes } = require("sequelize");
+const config = require("../config");
 
-const sequelize = new Sequelize(config.database.database, config.database.user, config.database.password, {
+const sequelize = new Sequelize(
+  config.database.database,
+  config.database.user,
+  config.database.password,
+  {
     host: config.database.host,
-    dialect: 'mysql',
-});
+    dialect: "mysql",
+  }
+);
 
-const User = sequelize.define('users', {
+const User = sequelize.define(
+  "users",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     employeePhoto: {
-        type: DataTypes.STRING,
-        allowNull: true,
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     employeeName: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
+      type: DataTypes.STRING(100),
+      allowNull: false,
     },
     employeeMobile: {
-        type: DataTypes.STRING(15),
-        allowNull: true,
+      type: DataTypes.STRING(15),
+      allowNull: true,
     },
     email: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        unique: true,
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
     },
     designation: {
-        type: DataTypes.STRING(50),
-        allowNull: true,
+      type: DataTypes.STRING(50),
+      allowNull: true,
     },
     password: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     role: {
-        type: DataTypes.ENUM('admin', 'user', 'sub_user', 'super_user'),
-        allowNull: false,
-    }, status: {
-        type: DataTypes.ENUM('Active', 'In Active'),
-        allowNull: false,
-    }
-}, {
-    tableName: 'users',
+      type: DataTypes.ENUM("admin", "user", "sub_user", "super_user"),
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM("Active", "In Active"),
+      allowNull: false,
+    },
+    login_expiry: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "users",
     timestamps: true,
-});
+  }
+);
 
 sequelize.sync();
 
